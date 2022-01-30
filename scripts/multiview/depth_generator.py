347d749a0f60e6b6f342780a04e1e9abb3a8ce2c
hazem youssef <hazem.youssef@tu-dortmund.de>
2022-01-30 22:09:04 +0100

    fixed scene id issue, generating depth directories, bop based depth image saving

--- scripts/multiview/depth_generator.py
+++ scripts/multiview/depth_generator.py
@@ -9,6 +9,7 @@ import os
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+
 p = {
   # See dataset_params.py for options.
   'dataset': 'mv',
@@ -46,15 +47,18 @@ for obj_id in dp_model['obj_ids']:
   ren.add_object(obj_id, model_fpath)
 
 scene_ids = dataset_params.get_present_scene_ids(dp_split)
+#for scene_id in tqdm(scene_ids):
 for scene_id in scene_ids:
-
+  scene_id_full = f'{scene_id:06d}'
+  depth_dir_path = os.path.join(dp_split['split_path'], scene_id_full, 'depth')
+  if not os.path.exists(depth_dir_path):
+    os.makedirs(depth_dir_path)
   # Load scene info and ground-truth poses.
   scene_camera = inout.load_scene_camera(
     dp_split['scene_camera_tpath'].format(scene_id=scene_id))
   scene_gt = inout.load_scene_gt(
     dp_split['scene_gt_tpath'].format(scene_id=scene_id))
 
-  scene_gt_info = {}
   im_ids = sorted(scene_gt.keys())
   for im_counter, im_id in enumerate(im_ids):
     if im_counter % 100 == 0:
@@ -63,21 +67,12 @@ for scene_id in scene_ids:
           p['dataset'], p['dataset_split'], p['dataset_split_type'], scene_id,
           im_id))
 
-    # Load depth image.
-    # depth_fpath = dp_split['depth_tpath'].format(scene_id=scene_id, im_id=im_id)
-    # if not os.path.exists(depth_fpath):
-    #   depth_fpath = depth_fpath.replace('.tif', '.png')
-    # depth = inout.load_depth(depth_fpath)
-    # depth *= scene_camera[im_id]['depth_scale']  # Convert to [mm].
-
     K = scene_camera[im_id]['cam_K']
     fx, fy, cx, cy = K[0, 0], K[1, 1], K[0, 2], K[1, 2]
     # im_size = (depth.shape[1], depth.shape[0])
 
     depth_imgs = []
-    scene_gt_info[im_id] = []
     for gt_id, gt in enumerate(scene_gt[im_id]):
-
       # Render depth image of the object model in the ground-truth pose.
       depth_gt_large = ren.render_object(
         gt['obj_id'], gt['cam_R_m2c'], gt['cam_t_m2c'],
@@ -87,18 +82,19 @@ for scene_id in scene_ids:
                    ren_cx_offset:(ren_cx_offset + im_width)]
       depth_imgs.append(depth_gt)
 
-        
+
     im_shape = np.shape(depth_gt)
     combined_depth_gt = np.zeros((im_shape[0], im_shape[1]))
     print('Combining depth images..')
-    for depth_img in tqdm(depth_imgs):
+    for depth_img in depth_imgs:
       rows, columns = np.where(depth_img > 0)
       for i, j in zip(rows, columns):
         if (combined_depth_gt[i, j] == 0):  # updated by first non-zero value in any depth image
           combined_depth_gt[i, j] = depth_img[i, j]
 
-    combined_depth_gt = np.asarray(combined_depth_gt, dtype=np.uint16)
-    cv2.imwrite(dp_split['depth_tpath'].format(scene_id=scene_id, im_id=im_id), combined_depth_gt)
+    inout.save_depth(dp_split['depth_tpath'].format(scene_id=scene_id, im_id=im_id), combined_depth_gt)
+    #combined_depth_gt = np.asarray(combined_depth_gt, dtype=np.uint16)
+    #cv2.imwrite(dp_split['depth_tpath'].format(scene_id=scene_id, im_id=im_id), combined_depth_gt)
     # cv2.imwrite('/home/hazem/projects/depth_test.png', depth_gt)
 
 
