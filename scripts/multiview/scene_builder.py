87e18c9afbbff59bd03f2e9de72e4b46b02b75fa
hazem youssef <hazem.youssef@tu-dortmund.de>
2022-01-30 22:19:54 +0100

    resolved scene id long/short format issues, resolved dict copying issue in GT json generation function

--- scripts/multiview/scene_builder.py
+++ scripts/multiview/scene_builder.py
@@ -11,7 +11,6 @@ import glob
 import shutil
 
 from bop_toolkit_lib.inout import save_scene_gt, save_scene_camera
-from depth_generator import generate_depth
 
 
 cams_ids = [0, 1]
@@ -26,12 +25,14 @@ calib_params_path = os.path.join(base_path, dataset_name, 'calib_params.csv')
 def copy_images(req_img_id, out_path):
     '''
         Modifies names of raw images and copies them to dataset directory
+        :param req_img_id: img id of interest (i.e: scene id) in long format
     '''
     for cam_id in cams_ids:
         imgs_path = os.path.join(recording_path, 'camera_'+str(cam_id), 'images')
         for img in sorted(glob.glob(os.path.join(imgs_path, '*.png'))):
             img_id = img.split('_')[-1].split('.')[-2]
-            if img_id == str(req_img_id):
+            req_img_id = str(int(req_img_id)) # convert id to short format
+            if img_id == req_img_id:
                 new_img_id = f'{cam_id:06d}' # use cam_id as img_id
                 shutil.copy2(os.path.join(imgs_path, 'image_'+str(img_id)+'.png'), os.path.join(out_path, str(new_img_id)+'.png'))
     print('Images copied')
@@ -69,15 +70,17 @@ def create_scene_gt(req_img_id, out_path):
     '''
         Combines GT values from all cameras for a particular 'snap' to create a json file
         Img_id: id of interest to concatenate from all cameras to build a scene
+        :param req_img_id: img id of interest (i.e: scene id) in long format
     '''
     out_dict = {}
     instances = 0
     for cam_id in cams_ids:
         cam_path = os.path.join(recording_path, 'camera_'+str(cam_id))
         gt_dict = read_gt_csv(cam_path)
-        img_id_mod = f'{req_img_id:06d}'
-        if img_id_mod in sorted(gt_dict.keys()):
-            out_dict[str(cam_id)] = gt_dict[str(img_id_mod)]  # img id is the cam_id (within a single scene)
+        gt_dict_copy = copy.deepcopy(gt_dict)
+        #img_id_mod = f'{req_img_id:06d}'
+        if req_img_id in sorted(gt_dict.keys()):
+            out_dict[str(cam_id)] = gt_dict_copy[str(req_img_id)]  # img_id is the cam_id (within a single scene)
             instances += 1
         #print((out_dict))
     if instances == 0:
@@ -102,9 +105,10 @@ def read_calib_params(calib_path):
             content['depth_scale'] = int(row[2])
             content_copy = copy.deepcopy(content)
             out_dict[row[0]] = content_copy
-        print(out_dict)
+        #print(out_dict)
     return out_dict
 
+# TODO: make finction independent from GT values and reliant on scene from folder structure
 def create_scene_camera(gt_dict, out_path):
     '''
         creates scene_camera.json for a scene using its GT values
@@ -115,6 +119,7 @@ def create_scene_camera(gt_dict, out_path):
         out_dict[img_id] = calib_params[img_id]
     out_path = os.path.join(out_path, 'scene_camera.json')
     save_scene_camera(out_path, out_dict)
+    return out_dict
 
 #TODO: replace mode with dataset split
 def build_scene(mode, scene_id):
@@ -123,29 +128,23 @@ def build_scene(mode, scene_id):
     :param mode: train or test
     :return:
     '''
+    scene_id = f'{scene_id:06d}'
     out_path = os.path.join(output_path, mode, scene_id)
-    print(out_path)
+    #print(out_path)
     if not os.path.exists(out_path):
         os.makedirs(out_path)
-    create_scene_gt(scene_id, out_path)
+    gt = create_scene_gt(scene_id, out_path) # scene_id corresponds (initially, berore modifying images with cam id) to the requested images id
     imgs_out_path = os.path.join(out_path, 'rgb')
     if not os.path.exists(imgs_out_path):
         os.makedirs(imgs_out_path)
     copy_images(scene_id, imgs_out_path)
-    #create_scene_camera(calib_params, out_path)
-    #generate_depth(dataset_name, mode, base_path)
-    #calc gt info
-    #calc_gt_masks
-
+    create_scene_camera(gt, out_path)
 
 def build_dataset(mode):
     max_len = get_max_len(recording_path) # accounts for images' filtration
     for i in range(max_len):
-        scene_id = f'{i:06d}'
         build_scene(mode, i)
     #create mode dis - test, train
-    #calc_model_info
-    #build scene
     pass
 
 def get_max_len(recording_path):
@@ -158,16 +157,17 @@ def get_max_len(recording_path):
         imgs_num = len(glob.glob(os.path.join(imgs_path, '*.png')))
         if imgs_num > max_val:
             max_val = imgs_num
-    print(max_val)
+    #print(max_val)
     return max_val
 
 if __name__ == '__main__':
     #cam_id = 0
     #cam_path = os.path.join(recording_path, 'camera_' + str(cam_id))
     #read_gt_csv(cam_path)
-    gt = create_scene_gt(0, '/home/hazem/projects/multi_view_dataset/mv/test/000001')
+    #gt = create_scene_gt(0, '/home/hazem/projects/multi_view_dataset/mv/test/000001')
     #test_dict()
     #copy_images(0, '/home/hazem/projects/multi_view_dataset/mv/test/000001')
     #get_max_len(recording_path)
     #build_scene('test')
-    create_scene_camera(gt, output_path)
+    #create_scene_camera(gt, output_path)
+    build_dataset('test')
