496ee16b8f3b7805c3e98b5a3a06d0a217a40561
Hazem Youssef <hazem.youssef@tu-dortmund.de>
2022-04-19 14:35:24 +0200

    restructured script; added reading calib params from csv; added save feature

--- scripts/multiview/overlay_imgs.py
+++ scripts/multiview/overlay_imgs.py
@@ -1,54 +1,53 @@
+import csv
+import ast
 import numpy as np
 import cv2
+import os
 
 
-mtx = np.array([[861.59229782,   0.        , 629.39824799],
-                [  0.        , 861.13244676, 526.14365127],
-                [  0.        ,   0.        ,   1.        ]])
-newcameramtx = np.array([[799.56201172,   0.        , 628.6002021 ],
-                         [  0.        , 799.69110107, 526.90323354],
-                         [  0.        ,   0.        ,   1.        ]])
-dist = np.array([[-0.16757778,  0.1080619 ,  0.00078127, -0.00021056, -0.01003737]])
-roi = (5, 11, 1287, 1001)
+calib_params_csv_file = './calib_params_all.csv'
 
-cam_locations_csv_file = '../cam_locations.csv'  # final csv file
-calib_params_csv_file = '../calib_params_all.csv'
+def get_calib_params(csv_file, cam_id):
+    with open(csv_file) as f:
+        reader = csv.reader(f)
+        for row in list(reader):
+            if row[0] == str(cam_id):
+                mtx = np.array(ast.literal_eval(row[1]))
+                newcameramtx = np.array(ast.literal_eval(row[2]))
+                dist = np.array(ast.literal_eval(row[3]))
+                roi = np.array(ast.literal_eval(row[4]))
+                return mtx, dist, newcameramtx, roi
 
-def undistort(img_path, mtx, dist, newcameramtx, roi, visualize):
-    '''
-    @param imgs_path: directory of images to be undistorted
-    @type imgs_path: str
-    ...
-    @return: image matrix
-    @rtype: np array
-    '''
-    print('Undistorting images')
-    img = cv2.imread(img_path)
+
+def undistort_img(img, mtx, dist, newcameramtx, roi):
     undistorted_img = cv2.undistort(img, mtx, dist, None, newcameramtx)
     # crop the image
     # x, y, w, h = roi
     # undistorted_img = undistorted_img[y:y+h, x:x+w]
     return undistorted_img
 
-img_rgb = '/media/athos/DATA-III/projects/multiview_dataset/mv/test/000000/rgb/000001.png'
-img_depth = '/media/athos/DATA-III/projects/multiview_dataset/mv/test/000000/depth/000001.png'
+def overlay_img(rgb_img_path, depth_img_path, cam_id, undistort=False):
+    rgb_img = cv2.imread(rgb_img_path)
+    depth_img = cv2.imread(depth_img_path)
+    if undistort:
+        mtx, dist, newcameramtx, roi = get_calib_params(calib_params_csv_file, cam_id)
+        rgb_img = undistort_img(rgb_img, mtx, dist, newcameramtx, roi)
+    res_img = np.add(rgb_img, depth_img)
+    while True:
+        cv2.imshow('overlay', res_img)
+        key = cv2.waitKey(0)
+        if key == 32: #press 'q' to exit
+            break
+        elif key == 115: #press 's' to save image
+            path = os.path.normpath(rgb_img_path).split('/')
+            img_id = int(path[-1].split('.')[-2])
+            scene_id = int(path[-3])
+            img_name = f'{scene_id:06d}_{img_id:06d}'
+            cv2.imwrite(f'./overlaid_imgs/{img_name}.png', res_img)
+        cv2.destroyAllWindows()
 
-#img_rgb = undistort(img_rgb, mtx, dist, newcameramtx, roi, False)
-img_rgb = cv2.imread(img_rgb)
-img_depth = cv2.imread(img_depth)
 
-#img_depth = np.fliplr(img_depth) ######
-
-print(np.shape(img_rgb))
-print(np.shape(img_depth))
-
-res_img = np.add(img_rgb, img_depth)
-print(np.shape(res_img))
-
-while True:
-    cv2.imshow('overlay', res_img)
-    key = cv2.waitKey(0)
-    if key == 32:
-        break
-
-    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    rgb_img_path = '/media/athos/DATA-III/projects/multiview_dataset/mv/test/000000/rgb/000001.png'
+    depth_img_path = '/media/athos/DATA-III/projects/multiview_dataset/mv/test/000000/depth/000001.png'
+    overlay_img(rgb_img_path, depth_img_path, cam_id=1, undistort=False) # only calibration parameters used in model projection
