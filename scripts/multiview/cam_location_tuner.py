34f04f3ef35b7fe77fbfb4b7db97d377f354454a
Hazem Youssef <hazem.youssef@tu-dortmund.de>
2022-06-06 22:45:21 +0200

    integrated script with object renderer at GT

--- scripts/multiview/cam_location_tuner.py
+++ scripts/multiview/cam_location_tuner.py
@@ -1,9 +1,9 @@
 import numpy as np
 import ast
 import csv
-from scipy.spatial.transform import Rotation as R
+from render_obj_at_gt import DepthGenerator
+from utils import convert_to_rot_mat, convert_to_euler
 from overlay_imgs import overlay_img
-from scene_processor import build_dataset
 import glob
 import os
 import cv2
@@ -12,6 +12,7 @@ import cv2
 
 intial_cam_locations_csv_file = 'initial_cam_locations.csv'
 final_cam_locations_csv_file = './accepted_cam_locations.csv'
+
 
 def get_initial_cam_location(cam_id):
     with open(intial_cam_locations_csv_file) as f:
@@ -22,17 +23,21 @@ def get_initial_cam_location(cam_id):
                 cam_rot_mat = ast.literal_eval(row[2])
     return cam_trans, cam_rot_mat
 
-def tune_cam_location(cam_id, trans_x_range, trans_y_range, trans_z_range, rot_x_range, rot_y_range, rot_z_range):
+def tune_cam_location(depth_generator, cam_id, trans_x_range, trans_y_range, trans_z_range, rot_x_range, rot_y_range, rot_z_range):
+    '''
+    trans/rot_range: desired amount of offsetting to the initial camera location
+    '''
+    ren = depth_generator.initialize_renderer()
     trans_x_inc = 1
     trans_y_inc = 1
     trans_z_inc = 1
-    rot_x_inc = 1
+    rot_x_inc = -1
     rot_y_inc = 1
     rot_z_inc = 1
 
     cam_initial_trans, cam_intial_rot_mat = get_initial_cam_location(cam_id)
     cam_initial_rot = convert_to_euler(cam_intial_rot_mat)
-    cam_dir = f'../images/camera_{cam_id}'
+    cam_dir = f'./location_tuner_images/camera_{cam_id}/images'
 
     for img in glob.glob(os.path.join(cam_dir, '*.png')):
         for trans_x in range(trans_x_range[0], trans_x_range[1], trans_x_inc):
@@ -43,9 +48,18 @@ def tune_cam_location(cam_id, trans_x_range, trans_y_range, trans_z_range, rot_x
                             for rot_z in range(rot_z_range[0], rot_z_range[1], rot_z_inc):
                                 new_cam_trans = [cam_initial_trans[0]+trans_x, cam_initial_trans[1]+trans_y, cam_initial_trans[2]+trans_z]
                                 new_cam_rot = [cam_initial_rot[0]+rot_x, cam_initial_rot[1]+rot_y, cam_initial_rot[2]+rot_z]
-                                build_dataset('./images')
-                                # depth_img = cv2.imread(...)
-                                # overlay_img(img, depth_img)
+                                print(new_cam_trans)
+                                print(new_cam_rot)
+                                img_id = os.path.split(img)[-1].split('.')[-2]
+                                depth_img = depth_gen.get_combined_depth_img(cam_id=cam_id, img_id=img_id, ren=ren,
+                                                                             cam2vicon_trans=new_cam_trans,
+                                                                             cam2vicon_rot=new_cam_rot)
+                                # convert depth image from 1 channel float64 to 3 channels uint8
+                                depth_img = depth_img.astype(np.uint8)
+                                depth_img = cv2.merge([depth_img, depth_img, depth_img])
+                                rgb_img = cv2.imread(img)
+                                overlay_img(rgb_img, depth_img, cam_id)
+                                # depth_generator.save_img(depth_img, './location_tuner_images/depth_imgs')
 
 
 def write_to_csv(out_csv_file, data_list):
@@ -54,12 +68,12 @@ def write_to_csv(out_csv_file, data_list):
         writer.writerow(data_list)
 
 
-def convert_to_euler(rot_mat):
-    angle = R.from_matrix(rot_mat)
-    euler_angle = angle.as_euler("XYZ", degrees=True)
-    return euler_angle
+if __name__ == '__main__':
+    calib_params_csv = './calib_params_all.csv'
+    models_path = './models'
+    im_size = (1296, 1024)
+    obj_ids = list(range(1, 5))
 
-def convert_to_rot_mat(euler_angle):
-    angle = R.from_euler("XYZ", euler_angle, degrees=True)
-    rot_mat = angle.as_matix()
-    return rot_mat
+    depth_gen = DepthGenerator(calib_params_csv, models_path, im_size, obj_ids)
+    tune_cam_location(depth_generator=depth_gen, cam_id=6, trans_x_range=[-2, 2], trans_y_range=[-2, 2],
+                      trans_z_range=[-2, 3], rot_x_range=[-2, -3], rot_y_range=[-2, 2], rot_z_range=[-2, 2])
